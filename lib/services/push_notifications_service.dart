f46389f9d051eb3f2d1fa7a5544d2c263ce231a8
Marlon <jmcano@sistecredito.com>
2022-09-05 11:58:52 -0500

    Siquiente

--- lib/services/push_notifications_service.dart
+++ lib/services/push_notifications_service.dart
@@ -1,15 +1,28 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationService {
   static FirebaseMessaging message = FirebaseMessaging.instance;
   static String? token;
+  static StreamController<String> messageStreamController =
+      StreamController.broadcast();
 
-  static Future<void> _onMessageHandler(RemoteMessage message) async {}
+  static Stream<String> get messagesStream => messageStreamController.stream;
 
-  static Future<void> _backgraundHandler(RemoteMessage message) async {}
+  static Future<void> _onMessageHandler(RemoteMessage message) async {
+    messageStreamController.add(message.notification?.title ?? 'no Title');
+    print('entre');
+  }
 
-  static Future<void> _onOpenMessageOpenApp(RemoteMessage message) async {}
+  static Future<void> _backgraundHandler(RemoteMessage message) async {
+    messageStreamController.add(message.notification?.title ?? 'no Title');
+  }
+
+  static Future<void> _onOpenMessageOpenApp(RemoteMessage message) async {
+    messageStreamController.add(message.notification?.title ?? 'no Title');
+  }
 
   static Future initializeApp() async {
     await Firebase.initializeApp();
@@ -18,5 +31,9 @@ class PushNotificationService {
     FirebaseMessaging.onBackgroundMessage(_backgraundHandler);
     FirebaseMessaging.onMessage.listen(_onMessageHandler);
     FirebaseMessaging.onMessageOpenedApp.listen(_onOpenMessageOpenApp);
+  }
+
+  static closeStreams() {
+    messageStreamController.close();
   }
 }
